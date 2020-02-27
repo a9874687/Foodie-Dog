15a5c8dd35639f8351a6bb5ebb113350f9eed464
Bryan <48666047+a9874687@users.noreply.github.com>
2020-02-27 18:11:41 +0800

    FoodieDog v4.3

--- js/index_style.js
+++ js/index_style.js
@@ -1,23 +1,30 @@
 $(document).ready(function () {
-  //-------------------手機漢堡選單
-  $(".showmenu").click(function () {
+
+  //-------------------手機漢堡選單-----------------------------//
+
+  $("#header>.container> section a img").click(function () {
     if($(window).width()<= 374){ 
-      $("#header > .container > ul li").slideToggle();
+      $("#header > .container > section > ul li").slideToggle();
     };
   });
-  //--------------------登入彈跳視窗關閉按鈕
+
+  //--------------------登入彈跳視窗關閉按鈕-----------------------------//
+
   $(".close").click(function () {
     $(".login-contet").hide();
   });
 
-//--------------------評分高低下拉式選單
+//--------------------評分高低下拉式選單-----------------------------//
+
   $('.critic').click(function () {
     event.preventDefault();
     $('.critic>a').toggleClass('color');
     $('.critic-open').toggleClass('border-radius-bottom color').slideToggle();
     $('.critic').toggleClass('border-radius-top color');
   });
-//--------------------最佳距離下拉式選單
+
+//--------------------最佳距離下拉式選單-----------------------------//
+
   $('.distance').click(function () {
     event.preventDefault();
     $('.distance>a').toggleClass('color');
@@ -25,14 +32,17 @@ $(document).ready(function () {
     $('.distance').toggleClass('border-radius-top color');
   });
 
-//--------------------每人均消下拉式選單
+//--------------------每人均消下拉式選單-----------------------------//
+
   $('.consumption').click(function () {
     event.preventDefault();
     $('.consumption>a').toggleClass('color');
     $('.consumption-open').toggleClass('border-radius-bottom color').slideToggle();
     $('.consumption').toggleClass('border-radius-top color');
   });
-//--------------------滑鼠移入更換大圖
+
+//--------------------滑鼠移入更換大圖-----------------------------//
+
   $('#storefront-photo img').hover(function () {
     $(this).addClass("active");
     $('#storefront img').attr("src", $(this).attr("src"));
@@ -40,7 +50,8 @@ $(document).ready(function () {
     $("#storefront-photo img").removeClass();
   });
 
-//--------------------登入頁面彈跳視窗
+//--------------------登入頁面彈跳視窗-----------------------------//
+
   $(function () {
     $('.login-btn').click(function () {
       $('.bg').fadeIn(200);
@@ -52,7 +63,8 @@ $(document).ready(function () {
     });
   });
 
-//--------------------評論頁面彈跳視窗
+//--------------------評論頁面彈跳視窗-----------------------------//
+
   $(function () {
     $('.write-comment span a').click(function () {
       $('.bg').fadeIn(200);
@@ -64,8 +76,32 @@ $(document).ready(function () {
     });
   });
 
-
+//--------------------ajax讀取其他頁面-----------------------------//
   
-
+$('.product-info').click(function () {
+  let x = $(this).index();
+  console.log(x);
+  
+  switch (x) {
+      case 0:
+          $('.restaurant-details').load("../ajax/product-search-list1.html");
+          break;
+      case 1:
+          $('.restaurant-details').load("../ajax/product-search-list2.html");
+          break;
+      case 2:
+          $('.restaurant-details').load("../ajax/product-search-list3.html");
+            break;
+      case 3:
+          $('.restaurant-details').load("../ajax/product-search-list4.html");
+            break;
+      case 4:
+          $('.restaurant-details').load("../ajax/product-search-list5.html");
+            break;
+      case 5:
+          $('.restaurant-details').load("../ajax/product-search-list6.html");
+            break;
+       }
+  });
 
 });
